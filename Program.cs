050e23c462e28d061fb61bbcf999c5cab26519fd
OliwiaG307 <oliwia.gebala@gmail.com>
2025-05-31 09:35:44 +0200

    zadanie 3

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
-﻿Console.WriteLine("Podaj liczbę:");
+﻿Console.WriteLine("Podaj pierwszą liczbę do pomnożenia:");
 string firstUserInput = Console.ReadLine();
 int firstNumber = int.Parse(firstUserInput);
 
-Console.WriteLine("Podaj drugą liczbę:");
+Console.WriteLine("Podaj drugą liczbę do pomnożenia:");
 string secondUserInput = Console.ReadLine();
 int secondNumber = int.Parse(secondUserInput);
 
-bool sum = firstNumber + secondNumber;
-Console.WriteLine($"Suma tych dwóch liczb wynosi {sum}.");
+Console.WriteLine("Podaj trzecią liczbę do pomnożenia:");
+string thirdUserInput = Console.ReadLine();
+int thirdNumber = int.Parse(thirdUserInput);
 
-// Write a C# Sharp program to print the sum of two numbers.
+int numberMultiplied = firstNumber + secondNumber + thirdNumber;
+
+Console.WriteLine($"Twój wynik to: {numberMultiplied}");
+
+//Write a C# Sharp program to print the output of the multiplication of three numbers entered by the user.
